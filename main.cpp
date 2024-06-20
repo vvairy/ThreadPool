e2dcf8291fa0f20fce4c179ed0fb4467fbac8fe1
Roman <MaksimovRoA@stud.kai.ru>
2024-06-20 11:22:10 +0300

    moved string decl

--- main.cpp
+++ main.cpp
@@ -27,16 +27,17 @@ bool static isPrime(uint64_t num)
     if (num <= 1) return false;
     if (num == 2 || num == 3) return true;
     if (num % 2 == 0 || num % 3 == 0) return false;
-    for (uint64_t i = 5; i <= std::sqrt(num); i += 6)
+    for (uint64_t i = 5; i <= sqrt(num); i += 6)
         if (num % i == 0 || num % (i + 2) == 0) return false;
+    return true;
 }
 
 int main() {
     ThreadPool pool(2);
 
+    std::string input;
     while (true)
     {
-        std::string input;
         std::getline(std::cin, input);
         if (input == "exit")
             break;
